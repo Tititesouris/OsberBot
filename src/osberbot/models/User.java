9df4deb7f42c5dff219ffaf8770029849aa8f032
Quentin Brault <osberbot@gmail.com>
2015-09-27 14:55:19 +0200

    Started trying to understand DTOs

---
+++ src/osberbot/models/User.java
@@ -0,0 +1,33 @@
+package osberbot.models;
+
+/**
+ * Created by Tititesouris on 27/09/2015.
+ */
+public class User {
+
+    private int id;
+
+    private String name;
+
+    public User(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
