65e9ca529c8ab9ba06c374e6395a6e6952c824da
Quentin Brault <osberbot@gmail.com>
2015-09-27 12:45:48 +0200

    Hello World

---
+++ src/osberbot/Main.java
@@ -0,0 +1,12 @@
+package osberbot;
+
+/**
+ * Created by Tititesouris on 27/09/2015.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+}
